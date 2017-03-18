ad923180d230872fe96d963bd862896da51fed40
georgan <angeorges90@hotmail.com>
2017-03-18 22:52:41 +0200

    Add files via upload

---
+++ Thesis/utitlities/evaluationMeasures/getrocs.m
@@ -0,0 +1,26 @@
+function [ tpr, fpr, thresh ] = getrocs( curve, td, thresh)
+%getrocs returns (tpr,fpr) pairs that can be used to create roc curves.
+% Input:
+%   curve: the curve to threshold.
+%   td: truth data, the same size as curve.
+%   thresh: vector of thresholds with N elements to threshold the curve.
+% 
+% Output:
+%   tpr, fpr: Nx1 vectors with true prositive rate and false positive rate,
+%   respectively.
+
+
+n = length(thresh);
+fpr = zeros(n,1);
+tpr = fpr;
+
+
+for i = 1:n
+    class = curve >= thresh(i);
+    fpr(i) = fprate(class,td);
+    [~,~,tpr(i)] = eval_apr(class,td);
+    
+end
+
+end
+
