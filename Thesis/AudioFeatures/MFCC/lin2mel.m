c58387eb1886c786ad55cd50bf1b9a060cbecafb
George <angeorges90@hotmail.com>
2017-10-27 15:58:42 +0300

    Removed Thesis

--- Thesis/AudioFeatures/MFCC/lin2mel.m
+++
@@ -1,9 +0,0 @@
-function [ m ] = lin2mel( f )
-%LIN2MEL linear to mel scale
-%   f: frequency in Hz
-%   m: frequency in Mel
-
-m = 2595*log10(f/700+1);
-
-end
-
--- Thesis/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains part of the code that was developed on MATLAB during my thesis at NTUA.
